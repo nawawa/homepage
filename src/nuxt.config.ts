5bc0b2179f0fac3f6ad8911899ead0e6c249adb3
nawawa <oonawa12@gmail.com>
2022-07-16 23:12:24 +0900

    Vuetify3を追加

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,5 +2,24 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  app:{
+    head: {
+      title: "My Site",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+      ],
+    },
+  },
+  build: {
+    transpile: ["vuetify"]
+  },
+  vite: {
+      define: {
+          "process.env.DEBUG": false
+      }
+  },
 })
