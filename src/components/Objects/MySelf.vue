6601f9dab5a5e048acf6298de2f8cdb5b8a71557
nawawa <oonawa12@gmail.com>
2022-07-17 14:46:49 +0900

    スキルの表記を追加

--- src/components/Objects/MySelf.vue
+++ src/components/Objects/MySelf.vue
@@ -1,29 +1,57 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
   <div>
     <h1><ruby>名和亮雅<rt>Ryoga Nawa</rt></ruby></h1>
+
+    <h3>基本情報</h3>
     <v-table>
-      <thead>
-        <tr>
-          <th class="text-left">
-          </th>
-          <th class="text-left">
-          </th>
-        </tr>
-      </thead>
       <tbody>
         <tr>
-          <td>生年月日</td>
+          <td class="left">生年月日</td>
           <td>1997年12月2日</td>
         </tr>
         <tr>
-          <td>最終学歴</td>
+          <td class="left">最終学歴</td>
           <td>2018年3月 新潟デザイン専門学校イラストレーション科卒業</td>
+        </tr>
+      </tbody>
+    </v-table>
+
+    <h3>スキル</h3>
+    <v-table>
+      <tbody>
+        <tr>
+          <td class="left">言語</td>
+          <td>PHP 8 / JavaScript ES2015以降</td>
+        </tr>
+        <tr>
+          <td class="left">フレームワーク</td>
+          <td>Laravel 8以降 / Vue.js（Nuxt）2系</td>
+        </tr>
+      </tbody>
+    </v-table>
+
+    <h3>独学のみ</h3>
+    <v-table>
+      <tbody>
+        <tr>
+          <td class="left">言語</td>
+          <td>Ruby 2.6</td>
+        </tr>
+        <tr>
+          <td class="left">フレームワーク</td>
+          <td>Ruby on Rails 6</td>
         </tr>
       </tbody>
     </v-table>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .left {
+    width: 180px !important;
+  }
+
+</style>
