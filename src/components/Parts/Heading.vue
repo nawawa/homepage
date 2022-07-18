90808253e33af605fcc252e726d1c58ffe9e726b
nawawa <oonawa12@gmail.com>
2022-07-18 13:48:55 +0900

    Googleフォントを適用した見出しを追加

---
+++ src/components/Parts/Heading.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>
+    <slot />
+  </h1>
+</template>
+
+<style lang="scss" scoped>
+  h1 {
+    font-size: 36px;
+    font-family: 'Montserrat', sans-serif;
+    letter-spacing: 0.18em;
+    font-weight: 400;
+  }
+</style>
